385ae56d9dd3860959fd28d5da5bd8131c9d63d5
tenshialk <mariacicera0981@gmail.com>
2025-08-07 12:42:40 +0000

    hh

--- app.py
+++ app.py
@@ -1,12 +1,1 @@
-from flask import Flask, render_template
-
-# Inicializando o Flask
-app = Flask(__name__)
-
-# Rotas principais
-@app.route('/')
-def capa():
-    return render_template('capa.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+from flask import
