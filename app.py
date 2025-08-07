b81d75f65ae1a188aee6c584451eb9d4ad7f3db6
tenshialk <mariacicera0981@gmail.com>
2025-08-07 12:25:19 +0000

    y

--- app.py
+++ app.py
@@ -1,21 +1,12 @@
 from flask import Flask, render_template
 
+# Inicializando o Flask
 app = Flask(__name__)
 
+# Rotas principais
 @app.route('/')
 def capa():
     return render_template('capa.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-@app.route('/principal')
-def principal():
-    return render_template('principal.html')
-
-
 if __name__ == '__main__':
     app.run(debug=True)
-
-
