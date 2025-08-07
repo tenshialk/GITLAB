cb82f82c85bf36be97d7a4cc37f797e820a068d4
tenshialk <mariacicera0981@gmail.com>
2025-08-07 11:11:31 +0000

    1

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def capa():
+    return render_template('capa.html')
 
 @app.route('/login')
 def login():
