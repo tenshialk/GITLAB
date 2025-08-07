48fe14e24eaea6991fec50cb01b9e8408a897e3a
tenshialk <mariacicera0981@gmail.com>
2025-08-07 13:29:52 +0000

    1

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-from flask import
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def capa():
+    return render_template('capa.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+@app.route('/contatos')
+def contatos():
+    return render_template('contatos.html')
